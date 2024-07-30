131325cbd4f802bbfa3387b94ec9e33162858bff
irinafilipenko <abyoo15532@gmail.com>
2024-07-30 18:00:17 +0200

    change login_model

--- lib/data/models/login_model.dart
+++ lib/data/models/login_model.dart
@@ -1,16 +1,33 @@
-class LoginModel {
-  final String email;
-  final String fullName;
-  final String avatarUrl;
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-  const LoginModel(
-      {required this.email, required this.fullName, required this.avatarUrl});
+part 'login_model.freezed.dart';
+part 'login_model.g.dart';
 
-  factory LoginModel.fromJson(Map<String, dynamic> json) {
-    return LoginModel(
-      email: json['email'],
-      fullName: json['name']['first'] + ' ' + json['name']['last'],
-      avatarUrl: json['picture']['large'],
-    );
-  }
+@freezed
+class LoginModel with _$LoginModel {
+  const factory LoginModel({
+    required String email,
+    required String fullName,
+    required String avatarUrl,
+  }) = _LoginModel;
+
+  factory LoginModel.fromJson(Map<String, dynamic> json) =>
+      _$LoginModelFromJson(json);
 }
+
+// class LoginModel {
+//   final String email;
+//   final String fullName;
+//   final String avatarUrl;
+//
+//   const LoginModel(
+//       {required this.email, required this.fullName, required this.avatarUrl});
+//
+//   factory LoginModel.fromJson(Map<String, dynamic> json) {
+//     return LoginModel(
+//       email: json['email'],
+//       fullName: json['name']['first'] + ' ' + json['name']['last'],
+//       avatarUrl: json['picture']['large'],
+//     );
+//   }
+// }
